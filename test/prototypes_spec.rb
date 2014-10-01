684f67e5d7f2fa15b4c2339b3595ab81abe04f9c
vmariano <m.vicent@gmail.com>
2014-10-01 03:33:05 -0300

    add test to check if properties can change

--- test/prototypes_spec.rb
+++ test/prototypes_spec.rb
@@ -1,12 +1,19 @@
 require_relative '../src/prototyped_object'
 
-describe 'Prototypes behavior' do
-
+describe 'Prototypes Attributes' do
 
   it 'Prototypes can add properties' do
     guerrero = PrototypedObject.new
     guerrero.set_property(:energia, 100)
     expect(guerrero.energia).to eq(100)
+  end
+
+  it 'Prototypes can change his values' do
+    guerrero = PrototypedObject.new
+    guerrero.set_property(:energia, 100)
+
+    guerrero.energia= 200
+    expect(guerrero.energia).to eq(200)
   end
 
 end
